b670f0b22a719248d17e16e2a85c1196a41e92f7
pkhamdee <pkhamdee@gmail.com>
2021-06-21 22:26:45 +0700

    change

--- ci/tasks/build-covid-data-image/run.sh
+++ ci/tasks/build-covid-data-image/run.sh
@@ -3,13 +3,17 @@
 set -euo pipefail
 
 echo ${kubeconfig} > kube.config
-export KUBECONFIG=./kube.config
 
+echo "-------"
 cat ./kube.config
 
-kubectl cluster-info
+# export KUBECONFIG=./kube.config
 
-cd app-src
-REVISION=$(git rev-parse HEAD)
-echo ${REVISION}
-kp -n images image patch covid-data --git-revision ${REVISION} -w
+
+
+# kubectl cluster-info
+
+# cd app-src
+# REVISION=$(git rev-parse HEAD)
+# echo ${REVISION}
+# kp -n images image patch covid-data --git-revision ${REVISION} -w
