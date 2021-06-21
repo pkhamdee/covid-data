b77aa239be48e19c493ab9f78507c6916a62d2e8
pkhamdee <pkhamdee@gmail.com>
2021-06-22 00:54:02 +0700

    change

---
+++ ci/tasks/deploy-covid-data/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+cat ./app-src/ci/tasks/build-covid-data-image/kubeconfig-template | sed "s/kubetoken/${kubetoken}/g" > kubeconfig
+
+export KUBECONFIG=$(pwd)/kubeconfig
+
+kubectl cluster-info
+
+cd app-src
+REVISION=$(git rev-parse HEAD)
+echo ${REVISION}
+kp -n default image patch covid-data --git-revision ${REVISION} -w
